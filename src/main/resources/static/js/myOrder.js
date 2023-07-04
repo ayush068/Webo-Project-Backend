ad3f3644b4a2e4381917b98f53f71d4ea0069c3c
Shreyas Shetty A S <shreyasas2975@gmail.com>
2023-07-04 23:32:37 +0530

    additional changes in order page

---
+++ src/main/resources/static/js/myOrder.js
@@ -0,0 +1,10 @@
+// Variables
+const rateBtn = document.getElementById("rate-btn")
+let rateContents = document.getElementById("rating-contents")
+rateBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if (rateContents.style.display = "none")
+    {
+        rateContents.style.display = "flex"
+    }
+})

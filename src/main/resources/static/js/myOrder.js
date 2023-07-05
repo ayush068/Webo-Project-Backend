e6258da7df9b7483d632a0e24d295c41eb141806
Shreyas Shetty A S <shreyasas2975@gmail.com>
2023-07-05 18:12:08 +0530

    add about-us my-info return-order pages with its css files and one background img

--- src/main/resources/static/js/myOrder.js
+++ src/main/resources/static/js/myOrder.js
@@ -3,8 +3,12 @@ const rateBtn = document.getElementById("rate-btn")
 let rateContents = document.getElementById("rating-contents")
 rateBtn.addEventListener("click",(e)=>{
     e.preventDefault();
-    if (rateContents.style.display = "none")
+    if (rateContents.style.display == "none")
     {
         rateContents.style.display = "flex"
+    }
+    else
+    {
+        rateContents.style.display = "none"
     }
 })
